9f2c7885a208b66351a69735327714a1d3fab464
Sanya Mathew <sanya.a.mathew@gmail.com>
2022-09-30 13:57:36 -0700

    new commit iiiiihhhh

---
+++ Sleepy/SleepyApp.swift
@@ -0,0 +1,18 @@
+//
+//  SleepyApp.swift
+//  Sleepy
+//
+//  Created by Bellevue on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
